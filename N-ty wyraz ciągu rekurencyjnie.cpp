180667fa266c8ccc12ee4c1d24bcf6f77476ecaf
SebastianKaczmarczyk <99686840+SebastianKaczmarczyk@users.noreply.github.com>
2023-12-04 09:33:03 +0100

    Create N-ty wyraz ciągu rekurencyjnie.cpp

---
+++ N-ty wyraz ciągu rekurencyjnie.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+double funkcja(int n);
+
+int main() {
+    int n;
+    cout << "Podaj nr wyrazu ciagu, ktorego wartosc chcesz policzyc: ";
+    cin >> n;
+
+    double wynik = funkcja(n);
+
+    cout << n << " wyraz ciagu ma wartosc " << wynik << endl;
+
+    return 0;
+}
+
+double funkcja(int n) {
+    if (n == 1) {
+        return 1;
+    }
+    else if (n == 2) {
+        return 0.5;
+    }
+    else {
+        return -funkcja(n - 1) * funkcja(n - 2);
+    }
+}
